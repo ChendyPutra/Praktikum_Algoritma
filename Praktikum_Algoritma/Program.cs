184a722438c605ac039691b76c5f6c0a2c59c833
Chendy Lundy Janitra Putra Ashari <chendy.lundy.ft21@mail.umy.ac.id>
2022-10-05 22:42:28 +0700

    Add project files.

---
+++ Praktikum_Algoritma/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Bublbe_Sort
+{
+    class Program
+    {
+        //Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+
+        // Fungsi / method untuk menerima masukan
+        public void read()
+        {
+            //menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan Banyaknya Elemen Pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray Dapat Mempunyai Maksimal 20 Elemen.\n");
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine("------------------------");
+            Console.WriteLine(" Masukkan Elemen Array");
+            Console.WriteLine("------------------------");
+
+            // Pengguna memasukkan elemen pada array
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+        }
+    }
+}
